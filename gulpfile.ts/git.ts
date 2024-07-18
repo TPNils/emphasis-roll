fe115546d40fd69955756747872644d36facce8e
Nils <nils.yseboodt@gmail.com>
2024-07-18 17:26:12 +0200

    testing updated buildtools

--- gulpfile.ts/git.ts
+++ gulpfile.ts/git.ts
@@ -1,62 +1,32 @@
-import * as fs from 'fs-extra';
 import * as chalk from 'chalk';
-import * as stringify from 'json-stringify-pretty-compact';
 import { context as githubContext } from '@actions/github';
-import { foundryManifest } from './foundry-manifest';
 import { cli } from './cli';
-import { args } from './args';
+import { Version } from './version';
+import { FoundryVTT } from './foundy-vtt';
 
 export class Git {
-  public async updateManifestForGithub({source, externalManifest}: {source: boolean, externalManifest: boolean}): Promise<void> {
-    const packageJson = fs.readJSONSync('package.json');
-    const manifest = foundryManifest.getManifest();
-    if (!manifest) {
-      throw new Error(chalk.red('Manifest JSON not found in the ./src folder'));
-    }
 
-    const githubRepository = await this.getGithubRepoName();
+  public static async setGithubLinks(manifest: FoundryVTT.Manifest.LatestVersion, isLatest: boolean): Promise<void> {
+    const githubRepository = await Git.getGithubRepoName();
     if (githubRepository == null) {
       throw new Error(chalk.red(`Git no github repository found.`));
     }
 
-    const currentVersion = await args.getCurrentVersion();
-    let targetVersion = await args.getNextVersion(currentVersion, true);
-    if (targetVersion == null) {
-      targetVersion = currentVersion;
-    }
-
-    if (targetVersion.startsWith('v')) {
-      targetVersion = targetVersion.substring(1);
-    }
-
-    console.log(`Updating version number to '${targetVersion}'`);
-
-    packageJson.version = targetVersion;
-
-    manifest.file.version = targetVersion;
-    manifest.file.url = `https://github.com/${githubRepository}`;
+    manifest.url = `https://github.com/${githubRepository}`;
     // When foundry checks if there is an update, it will fetch the manifest present in the zip, for us it points to the latest one.
     // The external one should point to itself so you can download a specific version
     // The zipped one should point to the latest manifest so when the "check for update" is executed it will fetch the latest
-    if (externalManifest) {
-      // Seperate file uploaded for github
-      manifest.file.manifest = `https://github.com/${githubRepository}/releases/download/v${targetVersion}/module.json`;
-    } else {
+    if (isLatest) {
       // The manifest which is within the module zip
-      manifest.file.manifest = `https://github.com/${githubRepository}/releases/download/latest/module.json`;
+      manifest.manifest = `https://github.com/${githubRepository}/releases/download/latest/module.json`;
+    } else {
+      // Seperate file uploaded for github
+      manifest.manifest = `https://github.com/${githubRepository}/releases/download/${manifest.version}/module.json`;
     }
-    manifest.file.download = `https://github.com/${githubRepository}/releases/download/v${targetVersion}/module.zip`;
-
-    fs.writeFileSync(
-      'package.json',
-      stringify(packageJson, {indent: '  '}),
-      'utf8'
-    );
-    await foundryManifest.saveManifest({overrideManifest: manifest.file, source: source});
-    
+    manifest.download = `https://github.com/${githubRepository}/releases/download/${manifest.version}/module.zip`;
   }
 
-  public async getGithubRepoName(): Promise<string> {
+  public static async getGithubRepoName(): Promise<string> {
     let githubRepository: string;
 
     // Try to detect the github repo in a github action
@@ -107,7 +77,7 @@ export class Git {
     return githubRepository;
   }
 
-  public async validateCleanRepo(): Promise<void> {
+  public static async validateCleanRepo(): Promise<void> {
     const cmd = await cli.execPromise('git status --porcelain');
     cli.throwIfError(cmd);
     if (typeof cmd.stdout === 'string' && cmd.stdout.length > 0) {
@@ -115,63 +85,48 @@ export class Git {
     }
   }
 
-  public async commitNewVersion(): Promise<void> {
+  public static async commitNewVersion(version: Version): Promise<void> {
     cli.throwIfError(await cli.execPromise('git add .'), {ignoreOut: true});
-    let newVersion = 'v' + await args.getCurrentVersion();
-    cli.throwIfError(await cli.execPromise(`git commit -m "Updated to ${newVersion}`));
+    cli.throwIfError(await cli.execPromise(`git commit -m "Updated to ${Version.toString(version)}`));
   }
 
-  public async deleteVersionTag(version?: string): Promise<void> {
-    if (version == null) {
-      version = 'v' + await args.getCurrentVersion();
-    }
+  public static async deleteVersionTag(version: Version): Promise<void> {
     // Ignore errors
-    await cli.execPromise(`git tag -d ${version}`);
-    await cli.execPromise(`git push --delete origin ${version}`);
+    await cli.execPromise(`git tag -d ${Version.toString(version)}`);
+    await cli.execPromise(`git push --delete origin ${Version.toString(version)}`);
   }
 
-  public async getCurrentLongHash(): Promise<string> {
+  public static async getCurrentLongHash(): Promise<string> {
     const hash = await cli.execPromise(`git rev-parse HEAD`);
     cli.throwIfError(hash);
     return hash.stdout?.split('\n')?.[0];
   }
 
-  public async tagCurrentVersion(): Promise<void> {
-    let version = 'v' + await args.getCurrentVersion();
-    cli.throwIfError(await cli.execPromise(`git tag -a ${version} -m "Updated to ${version}"`));
-    cli.throwIfError(await cli.execPromise(`git push origin ${version}`), {ignoreOut: true});
+  public static async tagCurrentVersion(version: Version): Promise<void> {
+    let versionStr = Version.toString(version);
+    cli.throwIfError(await cli.execPromise(`git tag -a ${versionStr} -m "Updated to ${versionStr}"`));
+    cli.throwIfError(await cli.execPromise(`git push origin ${versionStr}`), {ignoreOut: true});
   }
 
-  public async getLatestVersionTag(): Promise<string> {
+  public static async getLatestVersionTag(): Promise<Version> {
     const tagHash = await cli.execPromise('git show-ref --tags');
     const rgx = /refs\/tags\/(.*)/g;
     let match: RegExpExecArray;
-    const versions = [];
+    const versions: Version[] = [];
     while (match = rgx.exec(tagHash.stdout)) {
-      if (args.parseVersion(match[1]) != null) {
-        versions.push(match[1]);
-      }
+      try {
+        versions.push(Version.parse(match[1]));
+      } catch {/*ignore*/}
     }
     if (!versions.length) {
-      return 'v0.0.0';
+      return {major: 0, minor: 0, patch: 0};
     }
-    versions.sort(args.sortVersions);
+    versions.sort(Version.sort);
     return versions[versions.length - 1];
   }
 
-  public async push(): Promise<void> {
+  public static async push(): Promise<void> {
     cli.throwIfError(await cli.execPromise(`git push`));
   }
 
-  public async gitMoveTag() {
-    await this.deleteVersionTag();
-    await this.tagCurrentVersion();
-  }
-}
-
-export const git = new Git();
-for (let prop in git) {
-  if (typeof git[prop] === 'function') {
-    git[prop] = git[prop].bind(git);
-  }
 }
